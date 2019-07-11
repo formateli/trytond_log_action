33af249d764b928a3159e22faca3e15e2703a2d2
formateli <formateli@gmail.com>
2019-07-10 22:32:44 -0500

    Full functionality

--- log_action.py
+++ log_action.py
@@ -1,6 +1,6 @@
 #This file is part of log_action Tryton module. The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-
+from trytond.pool import Pool
 from trytond.model import fields
 from trytond.transaction import Transaction
 from trytond.ir.resource import ResourceMixin
@@ -8,6 +8,7 @@ from datetime import datetime
 
 __all__ = [
         'LogAction',
+        'write_log'
     ]
 
 
@@ -15,8 +16,8 @@ class LogAction(ResourceMixin):
     "Log Action"
     __name__ = "log_action" 
 
-    action = fields.Char('Action')
-    date = fields.DateTime('Date')
+    action = fields.Char('Action', readonly=True)
+    date = fields.DateTime('Date', readonly=True)
     user = fields.Many2One('res.user', 'User', readonly=True)
 
     @staticmethod
@@ -26,3 +27,21 @@ class LogAction(ResourceMixin):
     @staticmethod
     def default_user():
         return Transaction().user
+
+    @classmethod
+    def log(cls, action, objs):
+        User = Pool().get('res.user')
+        user = User(Transaction().user)
+        with Transaction().set_user(0):
+            for obj in objs:
+                log = cls(
+                    resource=obj.__class__.__name__ + ',' + str(obj.id),
+                    action=action,
+                    user=user
+                )
+                log.save()
+
+
+def write_log(action, objs):
+    Log = Pool().get('log_action')
+    Log.log(action, objs)
