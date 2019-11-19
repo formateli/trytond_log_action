aefeb88b6e70fb160f00726d9606a01fadf6ebe0
formateli <formateli@gmail.com>
2019-11-18 19:08:04 -0500

    Not global logs

--- README.rst
+++ README.rst
@@ -3,55 +3,40 @@ Log Action
 
 Write logs on models.
 
-This module can be used in two different ways:
+How to use:
 
-1.- Creating global logs:
+1.- Import **LogActionMixin** class and **write_log** function:
+    ::
+        from trytond.modules.log_action import LogActionMixin, write_log
 
-    - Import **write_log** function:
-      ::
-          from trytond.modules.log_action import write_log
+2.- Create a model that derives from **LogActionMixin** and add the resouce field (Many2One)
+    to map the model you want to log, use *__name__* same as the model you want to log plus
+    *'.log_action'* appended to the end:
+    ::
+        class MyLog(LogActionMixin):
+            "My Logs Model"
+            __name__ = "my.model.log_action" 
+            resource = fields.Many2One('my.model',
+                'My Model', ondelete='CASCADE', select=True)
 
-    - Add the logs field to the model.
-      ::
-          logs = fields.One2Many ('log_action', 'resource', 'Logs')
+3.- Add the logs field (One2Many) to the model you want to log.
+    ::
+        logs = fields.One2Many ('my.model.log_action', 'resource', 'Logs')
 
-    - Use the **write_log** function wherever you want,
-      passing as arguments the message and objects affected:
-      ::
-          write_log ('Some insteresting log', sales)
+4.- Use the **write_log** function wherever you want.
+    If **write_log** finds *my.model.log_action* model then it writes messages using it,
+    otherwise an error is raised.
 
-    - **write_log** writes the message using *write_log* module.
+    **write_log** has three parameters:
+        - **action**: The message to log. It should be translated if necesary.
+        - **obj**: Objects from which logs are written.
+        - **key** (optional): key for searching porpuses.
 
-
-2.- Using your own log model:
-
-    - Import **LogActionMixin** class and **write_log** function:
-      ::
-          from trytond.modules.log_action import LogActionMixin, write_log
-
-    - Create a model that derives from **LogActionMixin** and add the resouce field
-      to map the model you want to log, use *__name__* same as the model to log plus
-      *'.log_action'* appended to the end:
-      ::
-          class YourLog(LogActionMixin):
-              "Your Logs"
-              __name__ = "my.model.log_action" 
-              resource = fields.Many2One('my.model',
-                  'My Model', ondelete='CASCADE', select=True)
-
-    - Add the logs field to the model using your log model.
-      ::
-          logs = fields.One2Many ('my.model.log_action', 'resource', 'Logs')
-
-    - Use the **write_log** function wherever you want.
-      If **write_log** finds *my.model.log_action* model then it writes messages using it,
-      otherwise it writes messages using *log_action* model.
-
-    - **Views:**: You can define your own views for the log model or use the views defined
-      on *log_action* module:
-      ::
-          <field name="logs" colspan="4"
-              view_ids="log_action.log_view_tree,log_action.log_view_form"/>
+5.- **Views:**: You can define your own views for the log model or use the views defined
+    on *log_action* module:
+    ::
+        <field name="logs" colspan="4"
+            view_ids="log_action.log_view_tree,log_action.log_view_form"/>
 
 
 License
--- log_action.py
+++ log_action.py
@@ -4,6 +4,7 @@ from trytond.pool import Pool
 from trytond.model import ModelSQL, ModelView, fields
 from trytond.transaction import Transaction
 from datetime import datetime
+from trytond.exceptions import UserError
 
 __all__ = [
         'LogActionMixin',
@@ -13,6 +14,7 @@ __all__ = [
 
 
 class LogActionMixin(ModelSQL, ModelView):
+    key = fields.Char('Key', readonly=True)
     action = fields.Char('Action', readonly=True)
     date = fields.DateTime('Date', readonly=True)
     user = fields.Many2One('res.user', 'User', readonly=True)
@@ -38,12 +40,13 @@ class LogActionMixin(ModelSQL, ModelView):
         return obj.id
 
     @classmethod
-    def log(cls, action, objs):
+    def log(cls, action, objs, key):
         user = Transaction().user
         logs = []
         with Transaction().set_user(0):
             for obj in objs:
                 logs.append({
+                    'key': key,
                     'resource': cls._get_resource(obj),
                     'action': action,
                     'user': user
@@ -54,82 +57,24 @@ class LogActionMixin(ModelSQL, ModelView):
 
 class LogAction(LogActionMixin):
     "Log Action"
-    __name__ = "log_action" 
-
-    resource = fields.Reference('Resource', selection='get_models',
-        required=True, select=True)
-
-    @staticmethod
-    def get_models():
-        pool = Pool()
-        Model = pool.get('ir.model')
-        ModelAccess = pool.get('ir.model.access')
-        models = Model.search([])
-        access = ModelAccess.get_access([m.model for m in models])
-        return [(m.model, m.name) for m in models if access[m.model]['read']]
-
-    @classmethod
-    def check_access(cls, ids, mode='read'):
-        pool = Pool()
-        ModelAccess = pool.get('ir.model.access')
-        if ((Transaction().user == 0)
-                or not Transaction().context.get('_check_access')):
-            return
-        model_names = set()
-        with Transaction().set_context(_check_access=False):
-            for record in cls.browse(ids):
-                if record.resource:
-                    model_names.add(str(record.resource).split(',')[0])
-        for model_name in model_names:
-            checks = cls._convert_check_access(model_name, mode)
-            for model, check_mode in checks:
-                ModelAccess.check(model, mode=check_mode)
-
-    @classmethod
-    def _convert_check_access(cls, model, mode):
-        return [
-            (model, {'create': 'write', 'delete': 'write'}.get(mode, mode))]
-
-    @classmethod
-    def read(cls, ids, fields_names):
-        cls.check_access(ids, mode='read')
-        return super(LogAction, cls).read(ids, fields_names)
-
-    @classmethod
-    def delete(cls, records):
-        cls.check_access([a.id for a in records], mode='delete')
-        super(LogAction, cls).delete(records)
-
-    @classmethod
-    def write(cls, records, values, *args):
-        all_records = []
-        actions = iter((records, values) + args)
-        for other_records, _ in zip(actions, actions):
-            all_records += other_records
-        cls.check_access([a.id for a in all_records], mode='write')
-        super(LogAction, cls).write(records, values, *args)
-        cls.check_access(all_records, mode='write')
-
-    @classmethod
-    def create(cls, vlist):
-        records = super(LogAction, cls).create(vlist)
-        cls.check_access([r.id for r in records], mode='create')
-        return records
-
-    @staticmethod
-    def _get_resource(obj):
-        return obj.__class__.__name__ + ',' + str(obj.id)
+    __name__ = "log_action"
 
 
-def write_log(action, objs):
-    if not objs:
+def write_log(action, objs, key=None):
+    if not objs or not action:
         return
     pool = Pool()
-    model = objs[0].__class__.__name__
+    model = objs[0].__class__.__name__ + '.log_action'
     try:
-        # Try to use model own logs
-        Log = pool.get(model + '.log_action')
+        Log = pool.get(model)
     except KeyError:
-        # Use global logs
-        Log = pool.get('log_action')
-    Log.log(action, objs)
+        raise UserError(
+            gettext('log_action.model_not_found',
+                model=model,
+            ))
+    except Exception as e:
+        raise UserError(
+            gettext('log_action.log_action_error',
+                error=str(e),
+            ))
+    Log.log(action, objs, key)
